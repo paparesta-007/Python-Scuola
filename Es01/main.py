0fe33660aa8b24c1bf63f5effe055d838b8c8885
unknown <t.paparesta.3231@vallauri.net>
2025-02-06 11:27:49 +0100

    ese01 file

---
+++ Es01/main.py
@@ -0,0 +1,15 @@
+x=5
+y=7.4
+
+print(x*y)
+print(x+y)
+
+print(f"{x:.2f}")
+
+frutta_list=["mela","arancia","anguria","banana"]
+
+for index, value in enumerate(frutta_list):
+    print(f"Posizione {index}: {value}")
+
+# for i in range(0,100,2):
+#     print(i)
